18dd64cd8954c53839ea425a58063cbf698f321a
XzaLenart <moh.reza.darmawan1@gmail.com>
2025-03-10 15:43:26 -0700

    Update firebase.jsx

--- GlitchX-pra-beta/src/firebase/firebase.jsx
+++ GlitchX-pra-beta/src/firebase/firebase.jsx
@@ -6,13 +6,13 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeDUGLR3_rurHVEspxH4Y6GEQ-TLbqXfE",
-  authDomain: "glitchx-b2b4f.firebaseapp.com",
-  projectId: "glitchx-b2b4f",
-  storageBucket: "glitchx-b2b4f.firebasestorage.app",
-  messagingSenderId: "943025110605",
-  appId: "1:943025110605:web:1dd1b39ccdfe48a4b92604",
-  measurementId: "G-54G7FGRDTE"
+  apiKey: "xxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxx",
+  measurementId: "xxxxxxxxxxxxxxxxxx"
 };
 
 // Initialize Firebase
